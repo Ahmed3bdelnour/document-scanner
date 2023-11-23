5b9857e733e6b9a99e1123aaf3643b998fe5279c
aabdelnour <aabdelnour@uplandsoftware.com>
2023-11-23 20:56:15 +0200

    v7

--- src/app/document-scanner/document-scanner.service.ts
+++ src/app/document-scanner/document-scanner.service.ts
@@ -67,9 +67,9 @@ export class DocumentScannerService {
           // start processing.
           cap.read(src);
 
-          cv.imshow('canvasOutput', src);
-          // cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-          // cv.imshow('canvasOutput', dst);
+          this.highlightPaper(src, dst, {});
+
+          cv.imshow('canvasOutput', dst);
 
           // schedule the next one.
           let delay = 1000 / FPS - (Date.now() - begin);
@@ -83,4 +83,220 @@ export class DocumentScannerService {
       setTimeout(processVideo, 0);
     };
   };
+
+  highlightPaper(src: any, dst: any, options: any = {}) {
+    options = options || {};
+    options.color = options.color || 'orange';
+    options.thickness = options.thickness || 10;
+
+    src.copyTo(dst);
+
+    const maxContour = this.findPaperContour(src);
+
+    if (maxContour) {
+      const {
+        topLeftCorner,
+        topRightCorner,
+        bottomLeftCorner,
+        bottomRightCorner,
+      } = this.getCornerPoints(maxContour);
+
+      if (
+        topLeftCorner &&
+        topRightCorner &&
+        bottomLeftCorner &&
+        bottomRightCorner
+      ) {
+        const color = [0, 255, 0, 255];
+        cv.line(dst, topLeftCorner, topRightCorner, color, 1);
+        cv.line(dst, topRightCorner, bottomRightCorner, color, 1);
+        cv.line(dst, bottomRightCorner, bottomLeftCorner, color, 1);
+        cv.line(dst, bottomLeftCorner, topLeftCorner, color, 1);
+      }
+    }
+  }
+
+  /**
+   * Finds the contour of the paper within the image
+   * @param {*} img image to process (cv.Mat)
+   * @returns the biggest contour inside the image
+   */
+  findPaperContour(img: any) {
+    const imgGray = new cv.Mat();
+    cv.cvtColor(img, imgGray, cv.COLOR_RGBA2GRAY);
+
+    const imgBlur = new cv.Mat();
+    cv.GaussianBlur(
+      imgGray,
+      imgBlur,
+      new cv.Size(5, 5),
+      0,
+      0,
+      cv.BORDER_DEFAULT
+    );
+
+    const imgThresh = new cv.Mat();
+    cv.threshold(imgBlur, imgThresh, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU);
+
+    let contours = new cv.MatVector();
+    let hierarchy = new cv.Mat();
+
+    cv.findContours(
+      imgThresh,
+      contours,
+      hierarchy,
+      cv.RETR_CCOMP,
+      cv.CHAIN_APPROX_SIMPLE
+    );
+    let maxArea = 0;
+    let maxContourIndex = -1;
+    for (let i = 0; i < contours.size(); ++i) {
+      let contourArea = cv.contourArea(contours.get(i));
+      if (contourArea > maxArea) {
+        maxArea = contourArea;
+        maxContourIndex = i;
+      }
+    }
+
+    const maxContour = contours.get(maxContourIndex);
+
+    imgGray.delete();
+    imgBlur.delete();
+    imgThresh.delete();
+    contours.delete();
+    hierarchy.delete();
+    return maxContour;
+  }
+
+  /**
+   * Extracts and undistorts the image detected within the frame.
+   * @param {*} image image to process
+   * @param {*} resultWidth desired result paper width
+   * @param {*} resultHeight desired result paper height
+   * @param {*} cornerPoints optional custom corner points, in case automatic corner points are incorrect
+   * @returns `HTMLCanvasElement` containing undistorted image
+   */
+  extractPaper(
+    image: any,
+    resultWidth: any,
+    resultHeight: any,
+    cornerPoints: any
+  ) {
+    const canvas = document.createElement('canvas');
+
+    const img = cv.imread(image);
+
+    const maxContour = this.findPaperContour(img);
+
+    const {
+      topLeftCorner,
+      topRightCorner,
+      bottomLeftCorner,
+      bottomRightCorner,
+    } = cornerPoints || this.getCornerPoints(maxContour);
+    let warpedDst = new cv.Mat();
+
+    let dsize = new cv.Size(resultWidth, resultHeight);
+    let srcTri = cv.matFromArray(4, 1, cv.CV_32FC2, [
+      topLeftCorner.x,
+      topLeftCorner.y,
+      topRightCorner.x,
+      topRightCorner.y,
+      bottomLeftCorner.x,
+      bottomLeftCorner.y,
+      bottomRightCorner.x,
+      bottomRightCorner.y,
+    ]);
+
+    let dstTri = cv.matFromArray(4, 1, cv.CV_32FC2, [
+      0,
+      0,
+      resultWidth,
+      0,
+      0,
+      resultHeight,
+      resultWidth,
+      resultHeight,
+    ]);
+
+    let M = cv.getPerspectiveTransform(srcTri, dstTri);
+    cv.warpPerspective(
+      img,
+      warpedDst,
+      M,
+      dsize,
+      cv.INTER_LINEAR,
+      cv.BORDER_CONSTANT,
+      new cv.Scalar()
+    );
+
+    cv.imshow(canvas, warpedDst);
+
+    img.delete();
+    warpedDst.delete();
+    return canvas;
+  }
+
+  /**
+   * Calculates the corner points of a contour.
+   * @param {*} contour contour from {@link findPaperContour}
+   * @returns object with properties `topLeftCorner`, `topRightCorner`, `bottomLeftCorner`, `bottomRightCorner`, each with `x` and `y` property
+   */
+  getCornerPoints(contour: any) {
+    let rect = cv.minAreaRect(contour);
+    const center = rect.center;
+
+    let topLeftCorner;
+    let topLeftCornerDist = 0;
+
+    let topRightCorner;
+    let topRightCornerDist = 0;
+
+    let bottomLeftCorner;
+    let bottomLeftCornerDist = 0;
+
+    let bottomRightCorner;
+    let bottomRightCornerDist = 0;
+
+    for (let i = 0; i < contour.data32S.length; i += 2) {
+      const point = { x: contour.data32S[i], y: contour.data32S[i + 1] };
+      const dist = this.distance(point, center);
+      if (point.x < center.x && point.y < center.y) {
+        // top left
+        if (dist > topLeftCornerDist) {
+          topLeftCorner = point;
+          topLeftCornerDist = dist;
+        }
+      } else if (point.x > center.x && point.y < center.y) {
+        // top right
+        if (dist > topRightCornerDist) {
+          topRightCorner = point;
+          topRightCornerDist = dist;
+        }
+      } else if (point.x < center.x && point.y > center.y) {
+        // bottom left
+        if (dist > bottomLeftCornerDist) {
+          bottomLeftCorner = point;
+          bottomLeftCornerDist = dist;
+        }
+      } else if (point.x > center.x && point.y > center.y) {
+        // bottom right
+        if (dist > bottomRightCornerDist) {
+          bottomRightCorner = point;
+          bottomRightCornerDist = dist;
+        }
+      }
+    }
+
+    return {
+      topLeftCorner,
+      topRightCorner,
+      bottomLeftCorner,
+      bottomRightCorner,
+    };
+  }
+
+  distance(p1: { x: number; y: number }, p2: { x: number; y: number }) {
+    return Math.hypot(p1.x - p2.x, p1.y - p2.y);
+  }
 }
