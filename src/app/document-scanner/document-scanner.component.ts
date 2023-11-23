cf36811de227aeef8d9b6cfcf044c53fbf68aef3
aabdelnour <aabdelnour@uplandsoftware.com>
2023-11-23 04:50:50 +0200

    Test

--- src/app/document-scanner/document-scanner.component.ts
+++ src/app/document-scanner/document-scanner.component.ts
@@ -24,36 +24,36 @@ export class DocumentScannerComponent implements OnInit {
   scanDocument() {
     this.cameraStreaming = true;
 
-    setTimeout(() => {
-      const video = document.getElementById('videoInput') as HTMLVideoElement;
-      const src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-      const dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-      const cap = new cv.VideoCapture(video);
-      this.processVideo(src, dst, cap);
-    }, 0);
-  }
+    let video = document.getElementById('videoInput') as HTMLVideoElement;
+    let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+    let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+    let cap = new cv.VideoCapture(video);
 
-  processVideo(src: any, dst: any, cap: any) {
     const FPS = 30;
 
-    try {
-      if (!this.cameraStreaming) {
-        src.delete();
-        dst.delete();
-        return;
+    const processVideo = () => {
+      try {
+        if (!this.cameraStreaming) {
+          // clean and stop.
+          src.delete();
+          dst.delete();
+          return;
+        }
+        let begin = Date.now();
+        // start processing.
+        cap.read(src);
+        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+        cv.imshow('canvasOutput', dst);
+        // schedule the next one.
+        let delay = 1000 / FPS - (Date.now() - begin);
+        setTimeout(processVideo, delay);
+      } catch (err) {
+        console.error(err);
       }
+    };
 
-      const begin = Date.now();
-      // start processing.
-      cap.read(src);
-      cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-      cv.imshow('canvasOutput', dst);
-      // schedule the next one.
-      let delay = 1000 / FPS - (Date.now() - begin);
-      setTimeout(() => this.processVideo(src, dst, cap), delay);
-    } catch (err: any) {
-      console.error('Failed to open Camera: ' + err.message);
-    }
+    // schedule the first one.
+    setTimeout(processVideo, 0);
   }
 
   captureImage() {
