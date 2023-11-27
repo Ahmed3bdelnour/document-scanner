a8b2b8403e65b493b645453f974a817a80cf0ab5
aabdelnour <aabdelnour@uplandsoftware.com>
2023-11-27 16:17:01 +0200

    v21

--- src/app/document-scanner/document-scanner.service.ts
+++ src/app/document-scanner/document-scanner.service.ts
@@ -384,60 +384,50 @@ export class DocumentScannerService {
    * @returns object with properties `topLeftCorner`, `topRightCorner`, `bottomLeftCorner`, `bottomRightCorner`, each with `x` and `y` property
    */
   getCornerPoints(contour: any) {
-    // let rect = cv.minAreaRect(contour);
-    // const center = rect.center;
-    // let topLeftCorner;
-    // let topLeftCornerDist = 0;
-    // let topRightCorner;
-    // let topRightCornerDist = 0;
-    // let bottomLeftCorner;
-    // let bottomLeftCornerDist = 0;
-    // let bottomRightCorner;
-    // let bottomRightCornerDist = 0;
-    // for (let i = 0; i < contour.data32S.length; i += 2) {
-    //   const point = { x: contour.data32S[i], y: contour.data32S[i + 1] };
-    //   const dist = this.distance(point, center);
-    //   if (point.x < center.x && point.y < center.y) {
-    //     // top left
-    //     if (dist > topLeftCornerDist) {
-    //       topLeftCorner = point;
-    //       topLeftCornerDist = dist;
-    //     }
-    //   } else if (point.x > center.x && point.y < center.y) {
-    //     // top right
-    //     if (dist > topRightCornerDist) {
-    //       topRightCorner = point;
-    //       topRightCornerDist = dist;
-    //     }
-    //   } else if (point.x < center.x && point.y > center.y) {
-    //     // bottom left
-    //     if (dist > bottomLeftCornerDist) {
-    //       bottomLeftCorner = point;
-    //       bottomLeftCornerDist = dist;
-    //     }
-    //   } else if (point.x > center.x && point.y > center.y) {
-    //     // bottom right
-    //     if (dist > bottomRightCornerDist) {
-    //       bottomRightCorner = point;
-    //       bottomRightCornerDist = dist;
-    //     }
-    //   }
-    // }
-    // return {
-    //   topLeftCorner,
-    //   topRightCorner,
-    //   bottomLeftCorner,
-    //   bottomRightCorner,
-    // };
-
-    const contourRect = cv.boundingRect(contour);
-    const { x, y, width, height } = contourRect;
-
+    let rect = cv.minAreaRect(contour);
+    const center = rect.center;
+    let topLeftCorner;
+    let topLeftCornerDist = 0;
+    let topRightCorner;
+    let topRightCornerDist = 0;
+    let bottomLeftCorner;
+    let bottomLeftCornerDist = 0;
+    let bottomRightCorner;
+    let bottomRightCornerDist = 0;
+    for (let i = 0; i < contour.data32S.length; i += 2) {
+      const point = { x: contour.data32S[i], y: contour.data32S[i + 1] };
+      const dist = this.distance(point, center);
+      if (point.x < center.x && point.y < center.y) {
+        // top left
+        if (dist > topLeftCornerDist) {
+          topLeftCorner = point;
+          topLeftCornerDist = dist;
+        }
+      } else if (point.x > center.x && point.y < center.y) {
+        // top right
+        if (dist > topRightCornerDist) {
+          topRightCorner = point;
+          topRightCornerDist = dist;
+        }
+      } else if (point.x < center.x && point.y > center.y) {
+        // bottom left
+        if (dist > bottomLeftCornerDist) {
+          bottomLeftCorner = point;
+          bottomLeftCornerDist = dist;
+        }
+      } else if (point.x > center.x && point.y > center.y) {
+        // bottom right
+        if (dist > bottomRightCornerDist) {
+          bottomRightCorner = point;
+          bottomRightCornerDist = dist;
+        }
+      }
+    }
     return {
-      topLeftCorner: { x, y },
-      topRightCorner: { x: x + width, y },
-      bottomLeftCorner: { x, y: y + height },
-      bottomRightCorner: { x: x + width, y: y + height },
+      topLeftCorner,
+      topRightCorner,
+      bottomLeftCorner,
+      bottomRightCorner,
     };
   }
 
