f16ee456327b5c32ee968a750efdf6c2094a4383
aabdelnour <aabdelnour@uplandsoftware.com>
2023-12-07 00:46:35 +0200

    Fix memory

--- src/app/jscannify-document-scanner/model.ts
+++ src/app/jscannify-document-scanner/model.ts
@@ -5,142 +5,20 @@ export class WebScanner {
     this.cv = _cv;
   }
 
-  findPaperContour(img: any, contour: any) {
-    let imgGray;
-    let imgBlur;
-    let imgThresh;
-    let contours;
-    let hierarchy;
-
-    try {
-      console.log('findPaperContour execution: start - src image matrix ', img);
-
-      imgGray = new this.cv.Mat();
-      this.cv.cvtColor(img, imgGray, this.cv.COLOR_RGBA2GRAY);
-
-      console.log('findPaperContour execution: imgGray ', imgGray);
-
-      imgBlur = new this.cv.Mat();
-      this.cv.GaussianBlur(
-        imgGray,
-        imgBlur,
-        new this.cv.Size(5, 5),
-        0,
-        0,
-        this.cv.BORDER_DEFAULT
-      );
-
-      console.log('findPaperContour execution: imgBlur ', imgBlur);
-
-      imgThresh = new this.cv.Mat();
-      this.cv.threshold(
-        imgBlur,
-        imgThresh,
-        0,
-        255,
-        this.cv.THRESH_BINARY + this.cv.THRESH_OTSU
-      );
-
-      console.log('findPaperContour execution: imgThresh ', imgThresh);
-
-      contours = new this.cv.MatVector();
-      hierarchy = new this.cv.Mat();
-      this.cv.findContours(
-        imgThresh,
-        contours,
-        hierarchy,
-        this.cv.RETR_CCOMP,
-        this.cv.CHAIN_APPROX_SIMPLE
-      );
-
-      console.log('findPaperContour execution: contours ', contours);
-
-      let maxArea = 0;
-      let maxContourIndex = -1;
-
-      for (let i = 0; i < contours.size(); ++i) {
-        let contourArea = this.cv.contourArea(contours.get(i));
-        if (contourArea > maxArea) {
-          maxArea = contourArea;
-          maxContourIndex = i;
-        }
-      }
-
-      console.log(
-        'findPaperContour execution: maxArea, maxContourIndex ',
-        maxArea,
-        maxContourIndex
-      );
-
-      contour = maxContourIndex !== -1 ? contours.get(maxContourIndex) : null;
-
-      console.log('findPaperContour execution: maxContour ', contour);
-
-      this.deleteCVObject(imgGray);
-      this.deleteCVObject(imgBlur);
-      this.deleteCVObject(imgThresh);
-      this.deleteCVObject(contours);
-      this.deleteCVObject(hierarchy);
-
-      console.log(
-        'findPaperContour execution: cleaning matrixes',
-        imgGray,
-        imgBlur,
-        imgThresh,
-        contours,
-        hierarchy
-      );
-    } catch (error: any) {
-      this.deleteCVObject(imgGray);
-      this.deleteCVObject(imgBlur);
-      this.deleteCVObject(imgThresh);
-      this.deleteCVObject(contours);
-      this.deleteCVObject(hierarchy);
-
-      throw new Error(error);
-    }
-  }
-
   highlightPaper(capture: any, width: any, height: any) {
-    let img;
-    let maxContour;
+    let img = new this.cv.Mat(height, width, this.cv.CV_8UC4);
+    let maxContour = new this.cv.Mat();
 
     try {
-      console.log('highlightPaper execution: start');
-
-      img = new this.cv.Mat(height, width, this.cv.CV_8UC4);
       capture.read(img);
-
-      console.log('highlightPaper execution: src img martrix ', img);
-
-      this.findPaperContour(img, maxContour);
-
-      console.log('highlightPaper execution:  maxContour ', maxContour);
-
-      if (maxContour) {
-        this.drawContourInImage(img, maxContour);
-        this.deleteCVObject(maxContour);
-      }
-
+      const contourExists = this.findPaperContour(img, maxContour);
+      if (contourExists) this.drawContourInImage(img, maxContour);
+      this.deleteCVObject(maxContour);
       this.cv.imshow('result', img);
-
       this.deleteCVObject(img);
-
-      console.log(
-        'highlightPaper execution: cleaning img and maxContour ',
-        img,
-        maxContour
-      );
     } catch (error: any) {
       this.deleteCVObject(img);
       this.deleteCVObject(maxContour);
-
-      console.log(
-        'highlightPaper execution: cleaning img and maxContour ',
-        img,
-        maxContour
-      );
-
       throw new Error(error);
     }
   }
@@ -156,10 +34,10 @@ export class WebScanner {
     let img = this.cv.imread(image);
 
     if (!fullPaper) {
-      let maxContour;
-      this.findPaperContour(img, maxContour);
+      let maxContour = new this.cv.Mat();
+      const contourExists = this.findPaperContour(img, maxContour);
 
-      if (maxContour) {
+      if (contourExists) {
         let warpedDst = new this.cv.Mat();
 
         let dsize = new this.cv.Size(resultWidth, resultHeight);
@@ -191,13 +69,14 @@ export class WebScanner {
         this.cv.imshow(canvas, warpedDst);
 
         this.deleteCVObject(warpedDst);
-        this.deleteCVObject(maxContour);
         this.deleteCVObject(srcTri);
         this.deleteCVObject(dstTri);
-        this.deleteCVObject(M);
+        this.deleteCVObject(M); // delete is not needed?
       } else {
         this.cv.imshow(canvas, img);
       }
+
+      this.deleteCVObject(maxContour);
     } else {
       this.cv.imshow(canvas, img);
     }
@@ -207,14 +86,83 @@ export class WebScanner {
     return canvas;
   }
 
-  drawContourInImage(img: any, contour: any) {
-    let contoursToDraw;
-    let approxCurve;
+  findPaperContour(img: any, contour: any) {
+    let contourExists = false;
+    let imgGray = new this.cv.Mat();
+    let imgBlur = new this.cv.Mat();
+    let imgThresh = new this.cv.Mat();
+    let contours = new this.cv.MatVector();
+    let hierarchy = new this.cv.Mat();
 
     try {
-      approxCurve = new this.cv.Mat();
+      this.cv.cvtColor(img, imgGray, this.cv.COLOR_RGBA2GRAY);
+      this.cv.GaussianBlur(
+        imgGray,
+        imgBlur,
+        new this.cv.Size(5, 5),
+        0,
+        0,
+        this.cv.BORDER_DEFAULT
+      );
+      this.cv.threshold(
+        imgBlur,
+        imgThresh,
+        0,
+        255,
+        this.cv.THRESH_BINARY + this.cv.THRESH_OTSU
+      );
+      this.cv.findContours(
+        imgThresh,
+        contours,
+        hierarchy,
+        this.cv.RETR_CCOMP,
+        this.cv.CHAIN_APPROX_SIMPLE
+      );
+
+      let maxArea = 0;
+      let maxContourIndex = -1;
+
+      for (let i = 0; i < contours.size(); ++i) {
+        const c = contours.get(i);
+        let contourArea = this.cv.contourArea(c);
+        if (contourArea > maxArea) {
+          maxArea = contourArea;
+          maxContourIndex = i;
+        }
+        c.delete();
+      }
+
+      if (maxContourIndex !== -1) {
+        contourExists = true;
+        const c = contours.get(maxContourIndex);
+        c.copyTo(contour);
+        c.delete();
+      }
+
+      this.deleteCVObject(imgGray);
+      this.deleteCVObject(imgBlur);
+      this.deleteCVObject(imgThresh);
+      this.deleteCVObject(contours);
+      this.deleteCVObject(hierarchy);
+    } catch (error: any) {
+      this.deleteCVObject(imgGray);
+      this.deleteCVObject(imgBlur);
+      this.deleteCVObject(imgThresh);
+      this.deleteCVObject(contours);
+      this.deleteCVObject(hierarchy);
+
+      throw new Error(error);
+    }
+
+    return contourExists;
+  }
+
+  drawContourInImage(img: any, contour: any) {
+    let contoursToDraw = new this.cv.MatVector();
+    let approxCurve = new this.cv.Mat();
+
+    try {
       this.getApproximatePolyDBContour(contour, approxCurve);
-      contoursToDraw = new this.cv.MatVector();
       contoursToDraw.push_back(approxCurve);
       this.cv.drawContours(
         img,
@@ -223,7 +171,6 @@ export class WebScanner {
         new this.cv.Scalar(255, 255, 0, 255),
         2
       );
-
       this.deleteCVObject(contoursToDraw);
       this.deleteCVObject(approxCurve);
     } catch (error: any) {
@@ -235,7 +182,7 @@ export class WebScanner {
   }
 
   getApproximatePolyDBContour(contour: any, approximateCurve: any) {
-    const epsilon = 0.04 * this.cv.arcLength(contour, true); // adjust the epsilon value as needed
+    const epsilon = 0.04 * this.cv.arcLength(contour, true);
     this.cv.approxPolyDP(contour, approximateCurve, epsilon, true);
   }
 
