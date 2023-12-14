144faaf2acbbc289e2369999bdcb8ab8dd4e7453
aabdelnour <aabdelnour@uplandsoftware.com>
2023-12-14 20:45:41 +0200

    test build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
 
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DocumentScannerNewComponent } from './document-scanner-new/document-scanner-new.component';
 import { DocumentScannerComponent } from './document-scanner/document-scanner.component';
-import { ImagesRowComponent } from './images-row/images-row.component';
 
 // export class MyHammerConfig extends HammerGestureConfig {
 //   override overrides = <any>{
@@ -15,18 +14,12 @@ import { ImagesRowComponent } from './images-row/images-row.component';
 // }
 
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     DocumentScannerComponent,
     DocumentScannerNewComponent,
-      ImagesRowComponent
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    DragDropModule,
-    // HammerModule
   ],
+  imports: [BrowserModule, AppRoutingModule, DragDropModule, HammerModule],
   providers: [
     // {
     //   provide: HAMMER_GESTURE_CONFIG,
