b932a5d233e57563ba68e08523fab5d92271977b
aabdelnour <aabdelnour@uplandsoftware.com>
2024-03-29 02:30:51 +0200

    update build

--- src/app/document-scanner/document-scanner.component.ts
+++ src/app/document-scanner/document-scanner.component.ts
@@ -67,7 +67,9 @@ export class DocumentScannerComponent implements OnInit, OnDestroy {
   ngAfterViewInit(): void {
     loadOpenCV(
       {
-        asm: window.location.origin + '/assets/js/opencv/opencv.js',
+        asm:
+          window.location.origin +
+          '/document-scanner/assets/js/opencv/opencv.js',
       },
       () => {
         this.InitScanner();
